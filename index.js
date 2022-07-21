4933caa8a2aa6676eb24e935177e4dabcf45af8f
Kristina Voroteliak <kristina.hodges7@gmail.com>
2022-07-21 11:22:06 -0400

    commit tree 823385b4a29e483bd5fcf39885e0c1bf3e25170c

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+// function fetchBooks() {
+
+//   return fetch("https://anapioficeandfire.com/api/books")
+//     // .then(response => response.json())
+//     // .then(data => renderBooks(data))
+
+//     .then(function(response) {
+//       return response.json()
+//     })
+//     .then(function(data) {
+//       renderBooks(data)
+//     })
+
+// }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+// document.addEventListener('DOMContentLoaded', function() {
+//   fetchBooks();
+// });
+function fetchBooks() {
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((res) => res.json())
+    .then((data) => renderBooks(data));
+}
